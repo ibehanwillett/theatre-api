0263180f916a78e988cabc77fbbabf3bd5b17c75
ibehanwillett <imogenbdubs@gmail.com>
2023-12-12 15:34:19 +1000

    Added authorisation to all qualification routes

--- blueprints/qualification_bp.py
+++ blueprints/qualification_bp.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, request
 from config import db
+from flask_jwt_extended import jwt_required
+from auth import *
 from models.qualification import Qualification, QualificationSchema
 
 qualifications_bp = Blueprint('qualification', __name__, url_prefix='/qualifications')
 
 # Return a list of all Qualifications
 @qualifications_bp.route('/')
+@jwt_required()
 def all_qualifications():
     stmt = db.select(Qualification)
     qualifications = db.session.scalars(stmt).all()
@@ -13,7 +16,9 @@ def all_qualifications():
 
 # Create a new Qualification
 @qualifications_bp.route('/', methods=['POST'])
+@jwt_required()
 def create_qualification():
+    admin_only()
     qualification_info = QualificationSchema(exclude=['id']).load(request.json)
     qualification = Qualification(
         name=qualification_info['name'],
@@ -24,9 +29,10 @@ def create_qualification():
     return QualificationSchema().dump(qualification),201
 
 # Updating a Qualification
-
 @qualifications_bp.route('/<int:id>', methods=['PUT','PATCH'])
+@jwt_required()
 def update_qualification(id):
+    admin_or_committee_only()
     qualification_info = QualificationSchema(exclude=['id']).load(request.json)
     stmt = db.select(Qualification).filter_by(id=id)
     qualification = db.session.scalar(stmt)
@@ -40,13 +46,15 @@ def update_qualification(id):
     
 # Deleting a Qualification
 @qualifications_bp.route('/<int:id>', methods=['DELETE'])
+@jwt_required()
 def delete_qualification(id):
+    admin_only()
     stmt = db.select(Qualification).filter_by(id=id)
     qualification = db.session.scalar(stmt)
     if qualification: 
         db.session.delete(qualification)
         db.session.commit()
-        return {'sucess': 'Qualification successfully deleted'},200
+        return {'Sucess': 'Qualification successfully deleted'},200
     else:
-        return {'error':'Qualification not found'}, 404
+        return {'Error':'Qualification not found'}, 404
     
